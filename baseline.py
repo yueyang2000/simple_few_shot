1a9697a737490751b4b297e5a6cef91186c37d1b
Lizb6626 <1511532471@qq.com>
2021-05-22 17:45:28 +0800

    finetune

--- baseline.py
+++ baseline.py
@@ -6,6 +6,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 
 from model import BackBone
+from model import FineTuner
 from dataset import Caltech256 
 
 net = BackBone()
@@ -25,12 +26,40 @@ def get_all_features(split='train', load=True):
     return features, dset.get_labels()
 
 if __name__ == '__main__':
-    print('Machine Learning Baselines')
-    X_train, y_train = get_all_features(split='train')
-    X_test, y_test = get_all_features(split='test')
-    clf = KNeighborsClassifier(n_neighbors=10)
-    clf.fit(X_train, y_train)
-    print('KNN:', clf.score(X_test, y_test)) # 0.212
-    clf = SVC()
-    clf.fit(X_train, y_train)
-    print('SVC:', clf.score(X_test, y_test)) # 0.548
+    # print('Machine Learning Baselines')
+    # X_train, y_train = get_all_features(split='train')
+    # X_test, y_test = get_all_features(split='test')
+    # clf = KNeighborsClassifier(n_neighbors=10)
+    # clf.fit(X_train, y_train)
+    # print('KNN:', clf.score(X_test, y_test)) # 0.212
+    # clf = SVC()
+    # clf.fit(X_train, y_train)
+    # print('SVC:', clf.score(X_test, y_test)) # 0.548
+
+    model = FineTuner()
+
+    # train
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    criterion = torch.nn.CrossEntropyLoss()
+    train_data = Caltech256(split='train')
+    dataloader = torch.utils.data.DataLoader(dataset=train_data, batch_size=16, shuffle=True)
+    epochs = 32
+    for epoch in tqdm(range(epochs)):
+        for index, (img, label) in enumerate(dataloader):
+            pred = model(img)
+            loss = criterion(pred, label.long())
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+    # test
+    test_data = Caltech256(split='test')
+    dataloader = torch.utils.data.DataLoader(dataset=test_data, batch_size=len(test_data))
+    with torch.no_grad():
+        for index, (img, label) in enumerate(dataloader):
+            pred = model(img)
+            _, pred = torch.max(pred.data, 1)
+            total = len(test_data)
+            correct = (pred == label).sum().item()
+            acc = correct / total
+            print(f'Accuracy: {acc}')
