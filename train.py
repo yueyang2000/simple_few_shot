0630b087f708f5cb744afcc3d1079ef21832b101
xyd18 <xyd18@mails.tsinghua.edu.cn>
2021-05-24 16:38:09 +0800

    Generate model pairs

--- train.py
+++ train.py
@@ -1,17 +1,23 @@
 import os
+import random
 import numpy as np
+from tqdm import tqdm
 import torch
 import torch.nn as nn
 import torchvision.models as models
 import torchvision
 import torch.optim as optim
 from sklearn import svm
+from sklearn.metrics import mean_squared_error
 
 import argparse
 
 from model import ModelRegression, BackBone
 
-M = 100
+N = 10
+S = 5
+M = 10
+rps = [-2,-1,0,1,2]
 
 class BaseFeature:
     def __init__(self, split='base'):
@@ -24,44 +30,80 @@ class BaseFeature:
             with open(label_path) as f:
                 lines = f.readlines()
                 for idx, line in enumerate(lines):
-                    self.labels.append(int(line))
-                    if self.categorys2feature.get(idx) == None:
-                        self.categorys2feature[int(line)] = [self.features[idx]]
+                    label = int(line)
+                    self.labels.append(label)
+                    if label in self.categorys2feature:
+                        self.categorys2feature[label].append(self.features[idx])
                     else:
-                        self.categorys2feature[int(line)].append(self.features[idx])
+                        self.categorys2feature[label] = [self.features[idx]]
             self.labels = np.asarray(self.labels)  # (100000,)
+            self.len = len(self.labels)
 
     def get_base_features(self):
         return self.categorys2feature
 
     def get_neg_c_random_features(self, c, size):
-        pass
-        # TODO
-
-
+        features = []
+        for i in range(size):
+            idx = random.randint(0,self.len-1)
+            while self.labels[idx] == c:
+                idx = random.randint(0,self.len-1)
+            features.append(self.features[idx])
+        return features
 
 def generate_model_pairs():
     base_feature = BaseFeature()
+    # modle_pairs = ModelPairs()    
 
-    # Initialize SVM classifier
-    clf = svm.LinearSVC()
-
-    for c, c_feature in base_feature.get_base_features.items():
+    for c, c_feature in base_feature.get_base_features().items():
         # for each category c
-        features = c_feature
         Y = np.ones( (len(c_feature),), dtype=np.int16)
-        features.append(base_feature.get_neg_c_random_features(c, M))
         Y = np.append(Y, -np.ones( (M,), dtype=np.int16))
+        neg_c_features = base_feature.get_neg_c_random_features(c, M)
+        c_feature += neg_c_features
+        # print(features, Y)
+
         # large
-        clf.fit(features, Y)
-        # TODO: get w_star
+        print('===large===')
+        clf = svm.LinearSVC(max_iter=5000)
+        print(np.asarray(c_feature), Y)
+        print(np.asarray(c_feature).shape, Y.shape)
+        clf.fit(np.asarray(c_feature), Y)
+        w_star1 = clf.coef_
+        w_star2 = clf.intercept_
+        w_star3 = clf.classes_  # (2,) [-1  1]
+        print(w_star1.shape, w_star1)
+        print(w_star2.shape, w_star2)
+        print(w_star3.shape, w_star3)
 
         # small (size = 10)
+        print('===small===')
+        Y_small = np.ones( (N,), dtype=np.int16)
+        Y_small = np.append(Y_small, -np.ones( (M,), dtype=np.int16))
         for i in range(S):
+            c_features_small = []
+            random_idx = []
+            for j in range(N):
+                idx = random.randint(0,len(c_feature)-1)
+                while idx in random_idx:
+                    idx = random.randint(0,len(c_feature)-1)
+                random_idx.append(idx)
+            for idx in random_idx:
+                c_features_small.append(c_feature[idx])
+            c_features_small += neg_c_features
 
-            clf.fit(X_train_small, y_train_small)
-        # TODO: get w_0
-
+            for rp in rps:  # regularization parameters
+                clf_small = svm.LinearSVC(max_iter=5000, C=10**rp)
+                print(np.asarray(c_features_small), Y_small)
+                print(np.asarray(c_features_small).shape, Y_small.shape)
+                clf_small.fit(np.asarray(c_features_small), Y_small)
+                w_0_1 = clf.coef_
+                w_0_2 = clf.intercept_
+                w_0_3 = clf.classes_  # (2,) [-1  1]
+                print(w_0_1.shape, w_0_1)
+                print(w_0_2.shape, w_0_2)
+                print(w_0_3.shape, w_0_3)
+                print(mean_squared_error(w_star1, w_0_1))
     return
 
 
