fba8ed7a2e234d95e38bd08f3ed5c6a520879a28
Lizb6626 <1511532471@qq.com>
2021-05-27 15:49:41 +0800

    some changes

--- MRN.py
+++ MRN.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from model import ModelRegression
+from dataset import BaseFeature
 
 class mrnLoss(nn.Module):
     def __init__(self, lam=1.0):
@@ -9,9 +10,13 @@ class mrnLoss(nn.Module):
         self.lam = lam
         self.reg = nn.MSELoss(reduction='sum')
     
-    def forward(self, w0, w_star):
+    def forward(self, w0, w_star, c):
         reg_loss = self.reg(w0, w_star)
         # TODO: performance loss
+        for i in range(w0.shape[0]):
+            # pred = 
+            c_idx = c[i]
+            pass
         return reg_loss
 
 
